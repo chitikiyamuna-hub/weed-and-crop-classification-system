e937c7ef24719d9709d906d9e77a7cab795b4854
chitikiyamuna-hub <chitikiyamuna@gmail.com>
2025-11-11 14:17:01 +0530

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,18 +1,18 @@
 import os
 
 image_files = []
-# Change directory to where the images are extracted
-# Assumes the Kaggle data extract results in data/agri_data/data/
+# Change directory to where the images are extracted in the Darknet setup
 os.chdir(os.path.join("data", "agri_data/data")) 
 for filename in os.listdir(os.getcwd()):
-    # Check for image files and their corresponding label files (which aren't handled here but should exist)
+    # Collect all image files
     if filename.endswith(".jpeg"):
-        # The path should be relative to the Darknet root for the detector to find it
+        # Path relative to the Darknet root directory
         image_files.append("data/agri_data/data/" + filename) 
 os.chdir("..") # Move back to the 'data' directory
+
 with open("train.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
-    # outfile.close() # Closing is redundant when using 'with open(...)', but harmless
+
 os.chdir("..") # Move back to the Darknet root directory
